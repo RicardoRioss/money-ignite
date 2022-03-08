da15ed2798bbdcf9ce7e64cfa23d6b3dcd114726
Ricardo Rios <rickrios23@hotmail.com>
2022-03-08 17:43:44 -0300

    miragejs instalado e fake ipi com rota definida

--- src/components/TansactionsTable/idex.tsx
+++ src/components/TansactionsTable/idex.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
 import { Container } from './styles';
 
 export function TransactionsTable() {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+      .then(response => response.json())
+      // eslint-disable-next-line no-console
+      .then(data => console.log(data));
+  }, []);
   return (
     <Container>
       <table>
